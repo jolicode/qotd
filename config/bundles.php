f8d751d653022793d2786d43764d6e16166fc94c
Grégoire Pineau <lyrixx@lyrixx.info>
2022-12-26 16:11:47 +0100

    Updated PHP vendor

--- config/bundles.php
+++ config/bundles.php
@@ -11,5 +11,4 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
