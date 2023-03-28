53b28922783d1e25337c999fa644d68600625c14
Grégoire Pineau <lyrixx@lyrixx.info>
2023-03-28 07:31:34 +0200

    better modal system

---
+++ assets/controllers/modal-controller.js
@@ -0,0 +1,17 @@
+import { Controller } from '@hotwired/stimulus';
+import { Modal } from 'bootstrap';
+
+export default class extends Controller {
+    static values = {
+        options: Object,
+    }
+
+    connect() {
+        this.modal = new Modal(this.element, this.optionsValue);
+        this.modal.show();
+    }
+
+    disconnect() {
+        this.modal.hide();
+    }
+}
