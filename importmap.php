cfa3ee8a0522814ee171c5835a92179c9bce6f49
Grégoire Pineau <lyrixx@lyrixx.info>
2025-06-04 14:00:36 +0200

    updated frontend vendor

--- importmap.php
+++ importmap.php
@@ -26,16 +26,16 @@ return [
         'version' => '3.2.2',
     ],
     'chart.js/auto' => [
-        'version' => '4.4.8',
+        'version' => '4.4.9',
     ],
     '@hotwired/turbo' => [
-        'version' => '8.0.12',
+        'version' => '8.0.13',
     ],
     'stimulus-carousel' => [
         'version' => '5.0.1',
     ],
     'swiper/bundle' => [
-        'version' => '11.2.4',
+        'version' => '11.2.8',
     ],
     'ssr-window' => [
         'version' => '5.0.0',
@@ -44,30 +44,30 @@ return [
         'version' => '4.0.6',
     ],
     'bootstrap' => [
-        'version' => '5.3.3',
+        'version' => '5.3.6',
     ],
     '@popperjs/core' => [
         'version' => '2.11.8',
     ],
     'bootstrap/dist/css/bootstrap.min.css' => [
-        'version' => '5.3.3',
+        'version' => '5.3.6',
         'type' => 'css',
     ],
     'bootstrap-icons/font/bootstrap-icons.css' => [
-        'version' => '1.11.3',
+        'version' => '1.13.1',
         'type' => 'css',
     ],
     'swiper/swiper-bundle.min.css' => [
-        'version' => '11.2.4',
+        'version' => '11.2.8',
         'type' => 'css',
     ],
     'turbo-view-transitions' => [
         'version' => '0.3.0',
     ],
     'chart.js' => [
-        'version' => '4.4.8',
+        'version' => '4.4.9',
     ],
     '@kurkle/color' => [
-        'version' => '0.3.4',
+        'version' => '0.4.0',
     ],
 ];
